b01ea5faa4612fcb7430ede52ab2c042cdcd7040
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2022-02-06 21:02:41 +1300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
-user=whoami
-echo $user
-if [ $user -ne 0 ]
+echo $USER
+if [ $USER -ne root ]
   then echo "Please run as root"
   exit
-elif [ $user = 0 ]
+elif [ $USER = root ]
   then echo "hello root user"
 fi
