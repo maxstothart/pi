9bda9eb93066c59c4cb2d19c103194abd43db677
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2022-02-06 21:49:13 +1300

    Update setup.sh

--- matchbox/setup.sh
+++ matchbox/setup.sh
@@ -4,7 +4,7 @@ sudo apt install matchbox-keyboard -y
 sudo cp toggle-keyboard.sh /usr/bin/toggle-keyboard.sh
 sudo chmod +x /usr/bin/toggle-keyboard.sh
 sudo cp toggle-keyboard.desktop /usr/share/raspi-ui-overrides/applications/toggle-keyboard.desktop
-cp /etc/xdg/lxpanel/LXDE-pi/panels/panel /home/pi/.config/lxpanel/LXDE-pi/panels/panel
+cp /etc/xdg/lxpanel/LXDE-pi/panels/panel ~/.config/lxpanel/LXDE-pi/panels/panel
 echo "Plugin {
   type=launchbar
   Config {
