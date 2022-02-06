26ae451a46e7d7aed7fb53f9e8a0ff3c6754bdf1
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2022-02-06 20:51:36 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,35 @@
 #!/bin/sh
-    echo "Hello Max"
-    sudo whoami
-    echo "Starting load please be patient"
+if [ $EUID -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+echo "Starting load please be patient"
 
-    echo "starting initial setup"
-      # copy desktop files from save to application directory
-      cp .desktop/TFT.desktop ~/.local/share/applications/TFT.desktop
-      cp .desktop/HDMI.desktop ~/.local/share/applications/HDMI.desktop
-      cd /
+echo "starting initial setup"
+	# copy desktop files from save to application directory
+	cp .desktop/TFT.desktop ~/.local/share/applications/TFT.desktop
+	cp .desktop/HDMI.desktop ~/.local/share/applications/HDMI.desktop
+	cd /
 
-    echo "installing git libaries"
-     git clone https://github.com/waveshare/LCD-show.git
-      git clone https://github.com/markondej/fm_transmitter
-     git clone https://github.com/maxstothart2020/pirate-radio.git
+echo "installing git libaries"
+	git clone https://github.com/waveshare/LCD-show.git
+	git clone https://github.com/markondej/fm_transmitter
+	git clone https://github.com/maxstothart2020/pirate-radio.git
    
-    echo "setting up git repos"
-     cd fm_transmitter
-     make GPIO21=1
+echo "setting up git repos"
+	cd /fm_transmitter
+	make GPIO21=1
 
-    echo "installing programs and upgrades"
-     sudo apt install idle -y
-     sudo apt update -y
-     sudo apt upgrade -y
+echo "installing programs and upgrades"
+	sudo apt install idle -y
+	sudo apt update -y
+	sudo apt upgrade -y
   
-    echo "setting up LCD screen"
-     cd LCD-show
-     chmod +x LCD35-show
-     ./LCD35-show 270
+echo "setting up LCD screen"
+	cd LCD-show
+	chmod +x LCD35-show
+	./LCD35-show 270
   
-    echo "rebooting in 5"
-    sleep(5000)
-    echo "sudo reboot"
+echo "rebooting in 5"
+	sleep(5000)
+	echo "sudo reboot"
