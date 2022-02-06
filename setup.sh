d1806c880da0c79147d243575c4cfd23936027f1
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2022-02-06 20:18:08 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,13 @@ cp .desktop/TFT.desktop ~/.local/share/applications/TFT.desktop
 cp .desktop/HDMI.desktop ~/.local/share/applications/HDMI.desktop
 cd /
 git clone https://github.com/waveshare/LCD-show.git
+git clone https://github.com/markondej/fm_transmitter
+cd fm_transmitter
+make GPIO21=1
 sudo apt install idle -y
 sudo apt update -y
 sudo apt upgrade -y
+cd LCD-show
+chmod +x LCD35-show
+./LCD35-show 270
 sudo reboot
