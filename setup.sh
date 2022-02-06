e6c20a7cb092f7c1ce04905a92c9642cc2bb2b7e
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2022-02-06 20:10:36 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@ echo "Hello Max"
 sudo whoami
 echo "Starting load please be patient"
 echo "starting initial setup"
-cp .desktop/TFT.Desktop ~/.local/share/applications/TFT.desktop
-cp .desktop/HDMI.Desktop ~/.local/share/applications/HDMI.desktop
+cp ~/pi/.desktop/TFT.Desktop ~/.local/share/applications/TFT.desktop
+cp ~/pi/.desktop/HDMI.Desktop ~/.local/share/applications/HDMI.desktop
 cd /
 git clone https://github.com/waveshare/LCD-show.git
-sudo apt install idle
-sudo apt update
-sudo apt upgrade
+sudo apt install idle -y
+sudo apt update -y
+sudo apt upgrade -y
 sudo reboot
