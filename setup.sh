6f35d97355842f9518713f11cf6b2ed78da6a87d
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2022-02-06 20:35:18 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,32 @@
 echo "Hello Max"
 sudo whoami
 echo "Starting load please be patient"
+
 echo "starting initial setup"
-cp .desktop/TFT.desktop ~/.local/share/applications/TFT.desktop
-cp .desktop/HDMI.desktop ~/.local/share/applications/HDMI.desktop
-cd /
-git clone https://github.com/waveshare/LCD-show.git
-git clone https://github.com/markondej/fm_transmitter
-cd fm_transmitter
-make GPIO21=1
-sudo apt install idle -y
-sudo apt update -y
-sudo apt upgrade -y
-cd LCD-show
-chmod +x LCD35-show
-./LCD35-show 270
-sudo reboot
+  # copy desktop files from save to application directory
+  cp .desktop/TFT.desktop ~/.local/share/applications/TFT.desktop
+  cp .desktop/HDMI.desktop ~/.local/share/applications/HDMI.desktop
+  cd /
+
+echo "installing git libaries"
+  git clone https://github.com/waveshare/LCD-show.git
+  git clone https://github.com/markondej/fm_transmitter
+  git clone https://github.com/maxstothart2020/pirate-radio.git
+   
+echo "setting up git repos"
+  cd fm_transmitter
+  make GPIO21=1
+
+echo "installing programs and upgrades"
+  sudo apt install idle -y
+  sudo apt update -y
+  sudo apt upgrade -y
+  
+echo "setting up LCD screen"
+  cd LCD-show
+  chmod +x LCD35-show
+  ./LCD35-show 270
+  
+echo "rebooting in 5"
+sleep(5000)
+echo "sudo reboot"
