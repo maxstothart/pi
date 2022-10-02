38564b369003205fbd5c346325c1a6a300a72e34
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-10-02 20:14:35 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,29 +6,30 @@ else
 	echo "Starting load please be patient"
 
 	echo "starting initial setup"
-		# copy desktop files from save to application directory
-		cp .desktop/TFT.desktop /home/maxs/.local/share/applications/TFT.desktop
-		cp .desktop/HDMI.desktop /home/maxs/.local/share/applications/HDMI.desktop
-		cp .desktop/transmit.desktop /home/maxs/.local/share/applications/transmit.desktop
-		cd /
+		sudo apt update && sudo apt upgrade -y
+		
 
 	echo "installing git libaries"
-		git clone https://github.com/goodtft/LCD-show.git
 		git clone https://github.com/maxstothart/piradio.git
 		git clone https://github.com/markondej/fm_transmitter.git
 
-	echo "installing programs and upgrades"
-		sudo apt update -y
-		sudo apt install idle -y
-		sudo apt upgrade -y
-  
+	echo "Installing Home Assistant"
+		sudo apt-get install jq wget curl avahi-daemon udisks2 libglib2.0-bin network-manager dbus apparmor -y
+		curl -fsSL https://get.docker.com -o get-docker.sh
+		sudo sh get-docker.sh
+		sudo usermod -aG docker admin
+		wget https://github.com/home-assistant/os-agent/releases/download/1.2.2/os-agent_1.2.2_linux_armv7.deb
+		sudo dpkg -i os-agent_1.2.2_linux_armv7.deb
+		wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
+		sudo dpkg -i homeassistant-supervised.deb
+		
+	echo "Installing Webmin"
+  		sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python shared-mime-info
+		wget http://prdownloads.sourceforge.net/webadmin/webmin_1.994_all.deb
+		dpkg --install webmin_1.994_all.deb
+
     	echo "setting up Transmitter"
 		cd /piradio
 		chmod +x setup.sh
 		./setup.sh
-		
-	echo "setting up LCD screen then rebooting"
-		cd /LCD-show
-		chmod +x LCD35-show
-		./LCD35-show 270
 fi
